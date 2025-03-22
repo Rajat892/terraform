3091f8f796558ebf8cb84a0cb139beab300a004b
Rajat892 <kumar.rishu892@gmail.com>
2025-03-23 00:40:17 +0530

    added-condition-to-check-if-buckets-exist

--- bucket.tf
+++ bucket.tf
@@ -3,10 +3,15 @@ data "google_storage_bucket" "existing_bucket" {
 }
 
 resource "google_storage_bucket" "raw" {
-  count = data.google_storage_bucket.existing_bucket ? 0 : 1
+  count = length(try(data.google_storage_bucket.existing_bucket.id, [])) == 0
   project                     = var.project-name
   name                        = "${var.project-name}-dev"
   force_destroy               = false
   uniform_bucket_level_access = true
   location                    = var.regions
+}
+
+# Output the existing bucket information
+output "existing_bucket_info" {
+  value = try(data.google_storage_bucket.existing_bucket, null)
 }
