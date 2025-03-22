cdc27d5659d40209e15f5f193670627a76be00df
Rajat892 <kumar.rishu892@gmail.com>
2025-03-22 12:33:29 +0530

    removed gcs backend

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
-terraform {
-    backend "gcs" { 
-      bucket  = "terraform-state-cicdproject"
-      prefix  = "dev"
-    }
-}
+# terraform {
+#     backend "gcs" { 
+#       bucket  = "terraform-state-cicdproject"
+#       prefix  = "dev"
+#     }
+# }
 
 provider "google" {
-  project = var.project-name
-  region = var.regions
+  project     = var.project-name
+  region      = var.regions
   credentials = jsondecode(var.gcp_credentials_json)
 }
--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "raw" {
-  project = var.project-name
-  name = "${var.project-name}-dev"
-  force_destroy = false
+  project                     = var.project-name
+  name                        = "${var.project-name}-dev"
+  force_destroy               = false
   uniform_bucket_level_access = true
-  location = var.regions
+  location                    = var.regions
 }
